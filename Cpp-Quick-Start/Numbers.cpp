c47d551eedd9d7691be50165a377b3050ed902c1
Derek Diaz Correa <gigabit77@gmail.com>
2020-05-25 21:21:43 -0500

    Added comments

--- Cpp-Quick-Start/Numbers.cpp
+++ Cpp-Quick-Start/Numbers.cpp
@@ -3,8 +3,8 @@
 int main() {
 	//Simple App that gets an input and prints it to the console
 	int number;
-	std::cout << "Enter a number between 1 and 100: ";
-	std::cin >> number;
-	std::cout << "Here's the number you typed: " << number << std::endl;
+	std::cout << "Enter a number between 1 and 100: "; //Output to console
+	std::cin >> number; //Grab input from console
+	std::cout << "Here's the number you typed: " << number << std::endl; //Shows the value captured back in the console
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Cpp-Quick-Start
+# C++ Quick Start
+
+
+## Files
+| Filename        | Description           | Link  |
+| ------------- |:-------------:| -----:|
+| Numbers.cpp     | Shows a very simple app that Outputs into console and grabs input from the console. | [File](https://github.com/derek-diaz/Cpp-Quick-Start/blob/master/Cpp-Quick-Start/Numbers.cpp) |
